9e4220ae667a0803779efb2525ab4dabc212fc9a
Lucas Henderson <lh291@st-andrews.ac.uk>
2026-03-28 16:05:09 +0000

    remove package as it is stupid

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import blackjack.*;
+import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         GUI gui = new GUI();
+        sleep(5000);
+        System.out.println(gui.getDealerCards());
     }
 }
